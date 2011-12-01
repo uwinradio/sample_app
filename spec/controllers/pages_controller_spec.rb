fe788c9174ca3c71535d65d278330e0328f47b9e
Andrew Phung <uwinradio@gmail.com>
2011-12-01 16:22:56 -0500

    Done with editing

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -44,7 +44,7 @@ describe PagesController do
     end
   end
 
-  describe "GET 'help'" do
+ describe "GET 'help'" do
     it "should be successful" do
       get 'help'
       response.should be_success
@@ -53,7 +53,9 @@ describe PagesController do
     it "should have the right title" do
       get 'help'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Help")
+                        :content =>
+                          "Ruby on Rails Tutorial Sample App | Help")
     end
   end
+
 end
